380860602203db33ab4c63a816f79d929e0b02fc
bartosz Żuber <93491389+BartoszJakis@users.noreply.github.com>
2023-11-04 12:48:04 +0100

    x and y axis movement with gui added

--- src/main.cpp
+++ src/main.cpp
@@ -207,7 +207,10 @@ int main()
         glm::mat4 view          = glm::mat4(1.0f);
         glm::mat4 projection    = glm::mat4(1.0f);
 
-        model = glm::rotate(model,  glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+
+        model = glm::rotate(model, glm::radians((float)xAxis), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, glm::radians((float)yAxis), glm::vec3(0.0f, 1.0f, 0.0f));
+
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
